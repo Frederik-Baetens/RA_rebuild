30f8ac97f31a43b844a2794de3c4ffe0bf7c906d
Frederik Baetens <baetens.fr@gmail.com>
2022-02-15 03:09:38 +0100

    even more minimal example

--- subproj/src/main.rs
+++ subproj/src/main.rs
@@ -1,8 +1,4 @@
-#[tokio::main]
-async fn main() {
-    console_subscriber::ConsoleLayer::builder()
-    .server_addr(([127, 0, 0, 1], 5555))
-    .init();
+fn main() {
     println!("Hello, worldleb!");
     let mastr = get_string();
     println!("{mastr}");
